98a0b5fe5783047bddd439be89bebb83b712ab38
TEJA KUMAR VASAMSETTI <107473361+tejakumar07@users.noreply.github.com>
2024-07-27 05:24:09 +0000

    First Commit

--- problem18.py
+++ problem18.py
@@ -1,5 +1,6 @@
-# This is about removing the Duplicate elements in the List and also sorted in accending order
+# This is about removing the Duplicate elements in the List
 
+# Method -1 Using Set method
 
 arr = [5,4,13,7,9,1,3,7]
 
@@ -7,3 +8,26 @@ arr = [5,4,13,7,9,1,3,7]
 arr = list(set(arr))
 
 print(arr)
+
+# Method -2 Using List Comprehension  Enemurated method
+
+test_list = [1, 5, 3, 6, 3, 5, 6, 1]
+res = [i for n, i in enumerate(test_list) if i not in test_list[:n]]
+print("The list after removing duplicates:", res)
+
+# Method -3 Using LOrdered Dict
+
+from collections import OrderedDict
+test_list = [1, 5, 3, 6, 3, 5, 6, 1]
+unique_list = list(OrderedDict.fromkeys(test_list))
+print("The list after removing duplicates:", unique_list)
+
+# Method -4 
+
+test_list = [1, 3, 5, 6, 3, 5, 6, 1]
+res = []
+for x in test_list:
+    if x not in res:
+        res.append(x)
+print("The list after removing duplicates:", res)
+
